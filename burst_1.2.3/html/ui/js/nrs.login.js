baf57a522c7055d21536401c04f04832e483ca5c
daWallet <dawallet@burst-team.us>
2016-04-26 14:20:37 +0200

    new login background
    spider animation

--- burst_1.2.3/html/ui/js/nrs.login.js
+++ burst_1.2.3/html/ui/js/nrs.login.js
@@ -125,7 +125,7 @@ var NRS = (function(NRS, $, undefined) {
 				"offset": 10
 			});
 			return;
-		} else if (!NRS.isTestNet && password.length < 12 && $("#login_check_password_length").val() == 1) {
+		} else if (!NRS.isTestNet && password.length < 35 && $("#login_check_password_length").val() == 1) {
 			$("#login_check_password_length").val(0);
 			$("#login_error .callout").html($.t("error_passphrase_login_length"));
 			$("#login_error").show();
