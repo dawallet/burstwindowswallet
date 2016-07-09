7a37dcbb6194be18ad18e72a8de22e2d9162a3d1
daWallet <dawallet@burst-team.us>
2016-07-09 15:59:01 +0200

    lower priority for blockchain verification

--- burst_1.2.4/run_java_autodetect.bat
+++ burst_1.2.4/run_java_autodetect.bat
@@ -2,13 +2,13 @@
 echo Checking Path only
 for %%f in (javaw.exe) do if exist %%~$path:f (
   echo Java found at: %%~$path:f
-  set launch="BURST" %%~$path:f -cp burst.jar;lib\*;conf nxt.Nxt
+  set launch="BURST"  /belownormal %%~$path:f -cp burst.jar;lib\*;conf nxt.Nxt
   goto startup
   ) else (
   echo Not found in Path, Searching full C drive
   for /F "tokens=*" %%f in ('%systemroot%\system32\where /F /R C:\ javaw.exe') do (
    echo Java found at: %%f
-   set launch="BURST" %%f -cp burst.jar;lib\*;conf nxt.Nxt
+   set launch="BURST"  /belownormal %%f -cp burst.jar;lib\*;conf nxt.Nxt
    goto startup
     )
   )
